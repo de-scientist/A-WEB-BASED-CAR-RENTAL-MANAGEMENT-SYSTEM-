b27798ccab7fe0eb1acfe79cd065f0ffffa523bf
de-scientist <121663689+de-scientist@users.noreply.github.com>
2023-03-13 09:02:05 +0300

    Added files via upload

---
+++ about.php
@@ -0,0 +1,233 @@
+<?php  include "php/connection.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>About page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="images/maybach.jpg">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+    <!--navbar-->
+    <?php include "includes/navbar.php";  ?>
+        <!--navbar-->
+    
+    <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+          <div class="col-md-9 ftco-animate pb-5">
+          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
+            <h1 class="mb-3 bread">About Us</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section ftco-about">
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-md-6 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(images/about.jpg);">
+					</div>
+					<div class="col-md-6 wrap-about ftco-animate">
+	          <div class="heading-section heading-section-white pl-md-5">
+	          	<span class="subheading">About us</span>
+	            <h2 class="mb-4">Welcome to Carbook</h2>
+
+	           
+	            <p>We are a renown company in Kenya offering quality and affordable Car Hire in Muranga county.</p>
+	            <p>At CarBook we value you as a customer and at all times strive to provide the best service to all our clients. Our cars are in various classes they range from Saloons, wagons, 4WD, Rav4s, Mini Pajeros, V.I.P. cars like Mercedes Benz, Land cruiser VX and Range Rovers we also have Pick-Ups, Minibuses and Motor cycles  available for hire</p>
+	            <!-- <p><a href="#" class="btn btn-primary py-3 px-4">Search Vehicle</a></p> -->
+	          </div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="ftco-section ftco-intro" style="background-image: url(images/bg_3.jpg);">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row justify-content-end">
+					<div class="col-md-6 heading-section heading-section-white ftco-animate">
+            <h2 class="mb-3">Do You Want To Earn With Us? So Don't Be Late.</h2>
+            <a href="contact.php" class="btn btn-primary btn-lg">Contact Us</a>
+          </div>
+				</div>
+			</div>
+		</section>
+
+
+    <section class="ftco-section testimony-section bg-light">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-7 text-center heading-section ftco-animate">
+          	<span class="subheading">Testimonial</span>
+            <h2 class="mb-3">Happy Clients</h2>
+          </div>
+        </div>
+        <div class="row ftco-animate">
+          <div class="col-md-12">
+            <div class="carousel-testimony owl-carousel ftco-owl">
+              <div class="item">
+                <div class="testimony-wrap rounded text-center py-4 pb-5">
+                  <div class="user-img mb-2" style="background-image: url(images/person_1.jpg)">
+                  </div>
+                  <div class="text pt-4">
+                    <p class="mb-4">Fair deal amazing car.</p>
+                    <p class="name">Harrison Mbugua</p>
+                    <span class="position">Marketing Manager</span>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap rounded text-center py-4 pb-5">
+                  <div class="user-img mb-2" style="background-image: url(images/person_2.jpg)">
+                  </div>
+                  <div class="text pt-4">
+                    <p class="mb-4">Used this service in Mombasa, Diani, Watamu and malindi. Osborn Maja very polite. They have a vast range of vehicles in good condition at varied prices for my differing needs (2 pax - 7pax) and then moving locations</p>
+                    <p class="name">Sheryl</p>
+                    <span class="position">Interface Designer</span>
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="testimony-wrap rounded text-center py-4 pb-5">
+                  <div class="user-img mb-2" style="background-image: url(images/person_3.jpg)">
+                  </div>
+                  <div class="text pt-4">
+                    <p class="mb-4">Everything worked out wonderful. We had read about this company on a forum. The car was delivered to the hotel on time and the price is fair. We received a friendly briefing and the car itself was in perfect condition. We rented it on Mauritius. Any time again.</p>
+                    <p class="name">Anthony</p>
+                    <span class="position">UI Designer</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                <div class="testimony-wrap rounded text-center py-4 pb-5">
+                  <div class="user-img mb-2" style="background-image: url(images/person_1.jpg)">
+                  </div>
+                  <div class="text pt-4">
+                    <p class="mb-4">The service was impeccable, the car was delivered to the hotel perfectly and on time, the vehicle was in top condition, collection from the hotel was also uncomplicated and easy. Had our car in Mauritius for 9 days and loved it. Gladly again.</p>
+                    <p class="name">Maxwell</p>
+                    <span class="position">Web Developer</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                <div class="testimony-wrap rounded text-center py-4 pb-5">
+                  <div class="user-img mb-2" style="background-image: url(images/person_1.jpg)">
+                  </div>
+                  <div class="text pt-4">
+                    <p class="mb-4">While spending a week at Lesbos, we decided renting a car for 5 days. Using Maki Car Rental was very easy and swift - everything went as expected although none of the local cars we new. Very happy about the support and service at pickup and delivery.</p>
+                    <p class="name">Lucie Gatwiri</p>
+                    <span class="position">System Analyst</span>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-counter ftco-section img bg-light" id="section-counter">
+			<div class="overlay"></div>
+    	<div class="container">
+    		<div class="row">
+          <div class="col-md-6 col-lg-3 justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text text-border d-flex align-items-center">
+                <strong class="number" data-number="60">0</strong>
+                <span>Year <br>Experienced</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text text-border d-flex align-items-center">
+                <?php
+                //total cars in database
+                $query = "SELECT * FROM  cars_db";
+                $select_cars_no = mysqli_query($connection,$query);
+                $car_count = mysqli_num_rows($select_cars_no);
+
+                ?>
+                <strong class="number" data-number="<?php echo $car_count ?>">0</strong>
+                <span>Total <br>Cars</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text text-border d-flex align-items-center">
+                <strong class="number" data-number="2590">0</strong>
+                <span>Happy <br>Customers</span>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 justify-content-center counter-wrap ftco-animate">
+            <div class="block-18">
+              <div class="text d-flex align-items-center">
+                <strong class="number" data-number="67">0</strong>
+                <span>Total <br>Branches</span>
+              </div>
+            </div>
+          </div>
+        </div>
+    	</div>
+    </section>	
+
+
+   
+  <?php include 'includes/footer.php'; ?>
+    
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/bootstrap-datepicker.js"></script>
+  <script src="js/jquery.timepicker.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,63 @@
+<div style=" margin-bottom:40px; max-width:100%;overflow:hidden;color:red;width:100%;height: 500px; "><div id="google-maps-canvas" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=Muranga,+Kenya&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="embed-ded-maphtml" rel="nofollow" href="https://www.bootstrapskins.com/themes" id="make-map-data">premium bootstrap themes</a><style>#google-maps-canvas img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}</style></div>
+  <footer class="ftco-footer ftco-bg-dark ftco-section">
+<div class="container">
+  <div class="row mb-5">
+    <div class="col-md">
+      <div class="ftco-footer-widget mb-4">
+        <h2 class="ftco-heading-2"><a href="#" class="logo">Car<span>book</span></a></h2>
+        <p>We are a renown company in Kenya offering quality and affordable Car Hire in Murang'a county and all over the country.</p>
+        <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
+          <li class="ftco-animate"><a href="https://mobile.twitter.com/kenyacarhire"><span class="icon-twitter"></span></a></li>
+          <li class="ftco-animate"><a href="https://www.facebook.com/CarHireKenya/"><span class="icon-facebook"></span></a></li>
+          <li class="ftco-animate"><a href="https://www.instagram.com/carhire_nairobi/?hl=en"><span class="icon-instagram"></span></a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md">
+      <div class="ftco-footer-widget mb-4 ml-md-5">
+        <h2 class="ftco-heading-2">Information</h2>
+        <ul class="list-unstyled">
+          <li><a href="about.php" class="py-2 d-block">About</a></li>
+          <li><a href="services.php" class="py-2 d-block">Services</a></li>
+          <!-- <li><a href="#" class="py-2 d-block">Term and Conditions</a></li> -->
+          <!-- <li><a href="#" class="py-2 d-block">Best Price Guarantee</a></li> -->
+          <!-- <li><a href="#" class="py-2 d-block">Privacy &amp; Cookies Policy</a></li> -->
+        </ul>
+      </div>
+    </div>
+    <div class="col-md">
+       <div class="ftco-footer-widget mb-4">
+        <h2 class="ftco-heading-2">Customer Support</h2>
+        <ul class="list-unstyled">
+          <!-- <li><a href="#" class="py-2 d-block">FAQ</a></li> -->
+          <li><a href="#" class="py-2 d-block">Payment Option</a></li>
+          <!-- <li><a href="#" class="py-2 d-block">Booking Tips</a></li>
+          <li><a href="#" class="py-2 d-block">How it works</a></li> -->
+          <li><a href="contact.php" class="py-2 d-block">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md">
+      <div class="ftco-footer-widget mb-4">
+          <h2 class="ftco-heading-2">Have a Questions?</h2>
+          <div class="block-23 mb-3">
+            <ul>
+              <li><span class="icon icon-map-marker"></span><span class="text">Murang'a Town</span></li>
+              <li><a href="#"><span class="icon icon-phone"></span><span class="text">+254 729934671</span></a></li>
+              <li><a href="#"><span class="icon icon-envelope"></span><span class="text">markII@master.com</span></a></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-12 text-center">
+
+      <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">De-Scientist</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+    </div>
+  </div>
+</div>
+
+</footer>
